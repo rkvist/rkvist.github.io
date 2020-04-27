a24e9942506243ee3629a96b08d42782456c3d95
rkvist <rkvist22@gmail.com>
2020-04-27 10:20:18 +0200

    auto-commit

---
+++ weatherapp/precache-manifest.ca4b6d8d1fcb9cdd030b378ce3768c80.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b8bb872a3a052f0a4685163674be43",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "0e1659b9de71556d9153",
+    "url": "/weatherapp/static/css/main.f047a055.chunk.css"
+  },
+  {
+    "revision": "a930098688cacbb0e386",
+    "url": "/weatherapp/static/js/2.38e20696.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/weatherapp/static/js/2.38e20696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e1659b9de71556d9153",
+    "url": "/weatherapp/static/js/main.5ec16d1f.chunk.js"
+  },
+  {
+    "revision": "cead67a97ba5fd378aab",
+    "url": "/weatherapp/static/js/runtime-main.8079a0c8.js"
+  },
+  {
+    "revision": "57bbf08a06d8adedecb51ec89bff65fe",
+    "url": "/weatherapp/static/media/bg_amsterdam.57bbf08a.jpg"
+  },
+  {
+    "revision": "7d28ba0ddd45f3ce5e2bf7899342db90",
+    "url": "/weatherapp/static/media/bg_bangkok.7d28ba0d.jpg"
+  },
+  {
+    "revision": "14f69a192d1ce1274887728c577c8487",
+    "url": "/weatherapp/static/media/bg_barcelona.14f69a19.jpg"
+  },
+  {
+    "revision": "5e1923535b46e6e52d76a79eb89d523c",
+    "url": "/weatherapp/static/media/bg_berlin.5e192353.jpg"
+  },
+  {
+    "revision": "cbdac445bf5dab1dda64d5f1d1a136ed",
+    "url": "/weatherapp/static/media/bg_copenhagen.cbdac445.jpg"
+  },
+  {
+    "revision": "4aa31377799bf24d1ddb06f822757011",
+    "url": "/weatherapp/static/media/bg_delhi.4aa31377.jpg"
+  },
+  {
+    "revision": "999587af86f21630fbecebdc60e1af0c",
+    "url": "/weatherapp/static/media/bg_dubai.999587af.jpg"
+  },
+  {
+    "revision": "ee1f05a1df53c713254300e5e0677bb7",
+    "url": "/weatherapp/static/media/bg_hongkong.ee1f05a1.jpg"
+  },
+  {
+    "revision": "ca2a9c4e5f9c5e443cd42e514b153436",
+    "url": "/weatherapp/static/media/bg_london.ca2a9c4e.jpg"
+  },
+  {
+    "revision": "5e52eac9bf17246f7f2b917aed57359c",
+    "url": "/weatherapp/static/media/bg_losangeles.5e52eac9.jpg"
+  },
+  {
+    "revision": "6c0f8a1f4f5335686bd41d9635412d5e",
+    "url": "/weatherapp/static/media/bg_madrid.6c0f8a1f.jpg"
+  },
+  {
+    "revision": "6158de328c66b9d26aa035c7d45441b2",
+    "url": "/weatherapp/static/media/bg_melbourne.6158de32.jpg"
+  },
+  {
+    "revision": "5029eb16607f964f0cc67d9eba6446f2",
+    "url": "/weatherapp/static/media/bg_miami.5029eb16.jpg"
+  },
+  {
+    "revision": "ff22b101150372ce95adedddec54d939",
+    "url": "/weatherapp/static/media/bg_mumbai.ff22b101.jpg"
+  },
+  {
+    "revision": "79866ccc2d918a67fca2e76e4c5a719f",
+    "url": "/weatherapp/static/media/bg_newyorkcity.79866ccc.jpg"
+  },
+  {
+    "revision": "c0775d859a8cbca0c74151977604d6d1",
+    "url": "/weatherapp/static/media/bg_oslo.c0775d85.jpg"
+  },
+  {
+    "revision": "f6aa2908a2545a206ea64f10de4d5ec6",
+    "url": "/weatherapp/static/media/bg_paris.f6aa2908.jpg"
+  },
+  {
+    "revision": "37815f4d5eb6a251246e321511721f46",
+    "url": "/weatherapp/static/media/bg_rome.37815f4d.jpg"
+  },
+  {
+    "revision": "c31a365c55503c2978fed596d822879e",
+    "url": "/weatherapp/static/media/bg_singapore.c31a365c.jpg"
+  },
+  {
+    "revision": "a3a1ea18ba5d4a31ae056eb4b8766adb",
+    "url": "/weatherapp/static/media/bg_stockholm.a3a1ea18.jpg"
+  },
+  {
+    "revision": "4c801194a826e0e19f1b614a46a92386",
+    "url": "/weatherapp/static/media/bg_sydney.4c801194.jpg"
+  },
+  {
+    "revision": "dcfb1ac78e5c769962bbd11105c45f6c",
+    "url": "/weatherapp/static/media/bg_tokyo.dcfb1ac7.jpg"
+  },
+  {
+    "revision": "a411cda47247dd982b8d3b6546ddc882",
+    "url": "/weatherapp/static/media/weatherapp_logo.a411cda4.svg"
+  }
+]);
